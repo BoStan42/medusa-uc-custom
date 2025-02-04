53bd8b1faee43ee8807b460b885263caea290aad
v-darko <viktor@unitedchargers.com>
2025-02-04 19:58:10 +0100

    test variables 2

--- packages/admin-ui/ui/src/constants/medusa-backend-url.ts
+++ packages/admin-ui/ui/src/constants/medusa-backend-url.ts
@@ -3,4 +3,4 @@ export const MEDUSA_BACKEND_URL = process.env.MEDUSA_BACKEND_URL || 'http://loca
 export const MEDUSA_BACKEND_URL_NOSLASH = MEDUSA_BACKEND_URL.replace(/\/+$/, '');
 
 // Temporary fix for the backend URL
-export const BACKEND_URL = process.env.MEDUSA_ADMIN_BACKEND_URL;
+// export const BACKEND_URL = process.env.MEDUSA_ADMIN_BACKEND_URL;
